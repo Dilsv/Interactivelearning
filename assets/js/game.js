6c56df540257803a630eb3995df8ed433a60e627
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2023-12-03 17:37:51 +0000

    Add question and answers into js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,110 @@
 const questionItems = [
-    
+    {
+        qustion: "Tree",
+        answers: [
+          {text: "Forest", correct: true},
+          {text: "Clock", correct: false},
+          {text: "Peanut", correct: false},
+          {text: "Bicycle", correct: false}
+        ],
+      },
+      {
+        question: "Piano",
+        answers: [
+          {text: "Cloud", correct: false},
+          {text: "Jiraffe", correct: false},
+          {text: "Toolbox", correct: false},
+          {text: "Music", correct: true}
+        ],
+      },
+      {
+        question: "Bicycle",
+        answers: [
+          {text: "Pizza", correct: false},
+          {text: "Ride", correct: true},
+          {text: "Hammer", correct: false},
+          {text: "Ocean", correct: false}
+        ],
+      },
+      {
+        question: "Sunset",
+        answers: [
+          {text: "Horizon", correct: true},
+          {text: "Laptop", correct: false},
+          {text: "Giraffe", correct: false},
+          {text: "Calculator", correct: false}
+        ],
+      },
+      {
+        question: "Telescope",
+        answers: [
+          {text: "Lemon", correct: false},
+          {text: "Pillow", correct: false},
+          {text: "Football", correct: false},
+          {text: "Stars", correct: true}
+        ],
+      },
+      {
+        question: "Adventure",
+        answers: [
+          {text: "Faucet", correct: false},
+          {text: "Jigsaw", correct: true},
+          {text: "Giraffe", correct: false},
+          {text:"Piano", correct: false}
+        ],
+      },
+      {
+        question: "Recipe",
+        answers: [
+          {text: "Chair", correct: false},
+          {text: "Moon", correct: false},
+          {text: "Cooking", correct: true},
+          {text:"Television", correct: false}
+          ],
+      },
+      {
+        question: "Book",
+        answers: [
+        {text: "Sandal", correct: false},
+        {text: "Lemonade", correct: false},
+        {text: "Reading", correct: true},
+        {text: "Telescope", correct: false}
+        ],
+      },
+      {
+        question: "Puzzle",
+        answers: [
+        {text: "Pieces", correct: true},
+        {text: "Giraffe", correct: false},
+        {text: "Calendar", correct: false},
+        {text: "Castle", correct: false}
+        ],
+      },
+      {
+        question: "Ocean",
+        answers: [
+        {text: "Waves", correct: true},
+        {text: "Toolbox", correct: false},
+        {text: "Cactus", correct: false},
+        {text: "Notebook", correct: false}
+        ],
+      },
+      {
+        question: "Camera",
+        answers: [
+        {text: "Photography", correct: true},
+        {text: "Giraffe", correct: false},
+        {text: "Ice Cream", correct: false},
+        {text: "Sailboat", correct: false}
+        ],
+      },
+      {
+        question: "Compass",
+        answers: [
+        {text: "Direction", correct: true},
+        {text: "Toaster", correct: false},
+        {text: "Giraffe", correct: false},
+        {text: "Backpack", correct: false}
+        ],
+      },
 ]
