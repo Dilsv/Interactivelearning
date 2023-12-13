01e6601474f0e04a1996fad4281d738c8ed8bb21
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2023-12-13 16:59:52 +0000

    Fix bug relating to score increments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,136 +1,140 @@
 const questionBank = [
   {
-    question: "Tree",
-    answers: [
-      {text: "Forest", correct: true},
-      {text: "Clock", correct: false},
-      {text: "Peanut", correct: false},
-      {text: "Bicycle", correct: false}
-    ],
-  },
-  {
-    question: "Piano",
-    answers: [
-      {text: "Cloud", correct: false},
-      {text: "Jiraffe", correct: false},
-      {text: "Toolbox", correct: false},
-      {text: "Music", correct: true}
-    ],
-  },
-  {
-    question: "Bicycle",
-    answers: [
-      {text: "Pizza", correct: false},
-      {text: "Ride", correct: true},
-      {text: "Hammer", correct: false},
-      {text: "Ocean", correct: false}
-    ],
-  },
-  {
-    question: "Sunset",
-    answers: [
-      {text: "Horizon", correct: true},
-      {text: "Laptop", correct: false},
-      {text: "Giraffe", correct: false},
-      {text: "Calculator", correct: false}
-    ],
-  },
-  {
-    question: "Telescope",
-    answers: [
-      {text: "Lemon", correct: false},
-      {text: "Pillow", correct: false},
-      {text: "Football", correct: false},
-      {text: "Stars", correct: true}
-    ],
-  },
-  {
-    question: "Adventure",
-    answers: [
-      {text: "Faucet", correct: false},
-      {text: "Jigsaw", correct: true},
-      {text: "Giraffe", correct: false},
-      {text:"Piano", correct: false}
-    ],
-  },
-  {
-    question: "Recipe",
-    answers: [
-      {text: "Chair", correct: false},
-      {text: "Moon", correct: false},
-      {text: "Cooking", correct: true},
-      {text:"Television", correct: false}
+      question: "Tree",
+      answers: [
+          { text: "Forest", correct: true },
+          { text: "Clock", correct: false },
+          { text: "Peanut", correct: false },
+          { text: "Bicycle", correct: false },
       ],
   },
   {
-    question: "Book",
-    answers: [
-    {text: "Sandal", correct: false},
-    {text: "Lemonade", correct: false},
-    {text: "Reading", correct: true},
-    {text: "Telescope", correct: false}
-    ],
+      question: "Piano",
+      answers: [
+          { text: "Cloud", correct: false },
+          { text: "Jiraffe", correct: false },
+          { text: "Toolbox", correct: false },
+          { text: "Music", correct: true },
+      ],
   },
   {
-    question: "Puzzle",
-    answers: [
-    {text: "Pieces", correct: true},
-    {text: "Giraffe", correct: false},
-    {text: "Calendar", correct: false},
-    {text: "Castle", correct: false}
-    ],
+      question: "Bicycle",
+      answers: [
+          { text: "Pizza", correct: false },
+          { text: "Ride", correct: true },
+          { text: "Hammer", correct: false },
+          { text: "Ocean", correct: false },
+      ],
   },
   {
-    question: "Ocean",
-    answers: [
-    {text: "Waves", correct: true},
-    {text: "Toolbox", correct: false},
-    {text: "Cactus", correct: false},
-    {text: "Notebook", correct: false}
-    ],
+      question: "Sunset",
+      answers: [
+          { text: "Horizon", correct: true },
+          { text: "Laptop", correct: false },
+          { text: "Giraffe", correct: false },
+          { text: "Calculator", correct: false },
+      ],
   },
   {
-    question: "Camera",
-    answers: [
-    {text: "Photography", correct: true},
-    {text: "Giraffe", correct: false},
-    {text: "Ice Cream", correct: false},
-    {text: "Sailboat", correct: false}
-    ],
+      question: "Telescope",
+      answers: [
+          { text: "Lemon", correct: false },
+          { text: "Pillow", correct: false },
+          { text: "Football", correct: false },
+          { text: "Stars", correct: true },
+      ],
   },
   {
-    question: "Compass",
-    answers: [
-    {text: "Direction", correct: true},
-    {text: "Toaster", correct: false},
-    {text: "Giraffe", correct: false},
-    {text: "Backpack", correct: false}
-    ],
+      question: "Adventure",
+      answers: [
+          { text: "Faucet", correct: false },
+          { text: "Jigsaw", correct: true },
+          { text: "Giraffe", correct: false },
+          { text: "Piano", correct: false },
+      ],
   },
-
+  {
+      question: "Recipe",
+      answers: [
+          { text: "Chair", correct: false },
+          { text: "Moon", correct: false },
+          { text: "Cooking", correct: true },
+          { text: "Television", correct: false },
+      ],
+  },
+  {
+      question: "Book",
+      answers: [
+          { text: "Sandal", correct: false },
+          { text: "Lemonade", correct: false },
+          { text: "Reading", correct: true },
+          { text: "Telescope", correct: false },
+      ],
+  },
+  {
+      question: "Puzzle",
+      answers: [
+          { text: "Pieces", correct: true },
+          { text: "Giraffe", correct: false },
+          { text: "Calendar", correct: false },
+          { text: "Castle", correct: false },
+      ],
+  },
+  {
+      question: "Ocean",
+      answers: [
+          { text: "Waves", correct: true },
+          { text: "Toolbox", correct: false },
+          { text: "Cactus", correct: false },
+          { text: "Notebook", correct: false },
+      ],
+  },
+  {
+      question: "Camera",
+      answers: [
+          { text: "Photography", correct: true },
+          { text: "Giraffe", correct: false },
+          { text: "Ice Cream", correct: false },
+          { text: "Sailboat", correct: false },
+      ],
+  },
+  {
+      question: "Compass",
+      answers: [
+          { text: "Direction", correct: true },
+          { text: "Toaster", correct: false },
+          { text: "Giraffe", correct: false },
+          { text: "Backpack", correct: false },
+      ],
+  },
 ];
+const MAX_SCORE = 10;
+let correctScore = 0;
+let incorrectScore = 0;
 
 let questionNumber;
-let questionElement = document.getElementById("question"); 
+let questionElement = document.getElementById("question");
 let answerElement = document.getElementById("btn-box");
-let oldScore = parseInt(document.getElementById("correct-score-counter").innerText);
-let nextButton = document.getElementById("next-btn"); 
+let oldScore = parseInt(
+  document.getElementById("correct-score-counter").innerText
+);
+let nextButton = document.getElementById("next-btn");
 let playAgainButton = document.getElementById("play-again-btn");
-let shuffledQuestions = questionBank.sort(() => Math.random() - .5);
-
+// let shuffledQuestions = questionBank.sort(() => Math.random() - 0.5);
+let shuffledQuestions = null;
 
 // Function to start quiz or play again
 function startGame() {
-questionNumber = 0;
-oldScore = 0;
-document.getElementById("score-area").style.display = "block";
-shuffleQuestionBank();
+  questionNumber = 0;
+  oldScore = 0;
+  document.getElementById("score-area").style.display = "block";
+  shuffleQuestionBank();
 }
 
 // Function to shuffle questionBank array at the start of each game
-function shuffleQuestionBank () {
-let shuffledQuestions = questionBank.sort(() => Math.random() - .5);
-showQuestion ();
+function shuffleQuestionBank() {
+  shuffledQuestions = questionBank.sort(() => Math.random() - 0.5);
+  showQuestion();
 }
 
 function showQuestion() {
@@ -139,20 +143,19 @@ function showQuestion() {
   const currentQuestion = shuffledQuestions[questionNumber++];
   questionElement.innerHTML = `${questionNumber}. ${currentQuestion.question}`;
 
-  currentQuestion.answers.forEach(answer => {
-    const button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("btn", "answer-btn");
+  currentQuestion.answers.forEach((answer) => {
+      const button = document.createElement("button");
+      button.innerHTML = answer.text;
+      button.classList.add("btn", "answer-btn");
 
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
+      if (answer.correct) {
+          button.dataset.correct = answer.correct;
+      }
 
-    button.addEventListener("click", selectAnswer);
-    answerElement.appendChild(button);
+      button.addEventListener("click", selectAnswer);
+      answerElement.appendChild(button);
   });
 }
-
 
 // Function to check answer, display feedback, and show Next button
 function selectAnswer(e) {
@@ -160,57 +163,63 @@ function selectAnswer(e) {
   const isCorrect = selectedButton.dataset.correct === "true";
 
   applyAnswerStyles(selectedButton, isCorrect);
-  disableAllAnswerButtons();
+  // disableAllAnswerButtons();
   showNextButton();
 }
 
 function applyAnswerStyles(button, isCorrect) {
   button.classList.add(isCorrect ? "correct" : "incorrect");
-  isCorrect ? incrementCorrect() : incrementIncorrect();
+  if (isCorrect) {
+    incrementCorrect();
+  } else {
+    incrementIncorrect();
+  }
 
-  Array.from(answerElement.children).forEach(btn => {
-    if (btn.dataset.correct === "true") {
-      btn.classList.add("correct");
-    }
-    btn.disabled = true;
+  Array.from(answerElement.children).forEach((btn) => {
+      if (btn.dataset.correct === "true") {
+          btn.classList.add("correct");
+      }
+      btn.disabled = true;
   });
 }
 
-function disableAllAnswerButtons() {
-  Array.from(answerElement.children).forEach(button => {
-    button.disabled = true;
-  });
-}
+// function disableAllAnswerButtons() {
+//     Array.from(answerElement.children).forEach((button) => {
+//         button.disabled = true;
+//     });
+// }
 
 function showNextButton() {
   nextButton.style.display = "block";
 }
 
-
-// Function to remove answer buttons, next button, play again button and score-comment
+/**
+* Remove answer buttons, next button, play again button and score-comment
+*/
 function resetState() {
-nextButton.style.display = "none";
-playAgainButton.style.display = "none";
-document.getElementById("score-comment").style.display = "none";
+  nextButton.style.display = "none";
+  playAgainButton.style.display = "none";
+  document.getElementById("score-comment").style.display = "none";
 
-while (answerElement.firstChild) {
-  answerElement.removeChild(answerElement.firstChild);
-}
+  while (answerElement.firstChild) {
+      answerElement.removeChild(answerElement.firstChild);
+  }
 }
 
 // Function to limit the quiz to 10 questions and then show score
-function nextQuestion () {
-if (questionNumber < 10) {
-showQuestion();
-} else {
-showScore();
-}}
+function nextQuestion() {
+  if (questionNumber < MAX_SCORE) {
+      showQuestion();
+  } else {
+      showScore();
+  }
+}
 
 // Function to show score, score-comment and play again button
 function showScore() {
   resetState();
 
-  const score = oldScore;
+  const score = correctScore;
   const scoreElement = document.getElementById("score-area");
   const commentElement = document.getElementById("score-comment");
 
@@ -218,50 +227,58 @@ function showScore() {
   scoreElement.style.display = "none";
   commentElement.style.display = "block";
 
-  if (score === 10) {
-    commentElement.innerHTML = "Congratulations! You are a matching Genius!";
+  if (score === MAX_SCORE) {
+      commentElement.innerHTML =
+          "Congratulations! You are a matching Genius!";
   } else if (score >= 7 && score <= 9) {
-    commentElement.innerHTML = "Wow! You smashed it! Keep playing!";
+      commentElement.innerHTML = "Wow! You smashed it! Keep playing!";
   } else if (score >= 1 && score <= 6) {
-    commentElement.innerHTML = "Great effort! Keep playing!";
+      commentElement.innerHTML = "Great effort! Keep playing!";
   } else {
-    commentElement.innerHTML = "Let's try beating the highscore! Keep going!";
+      commentElement.innerHTML =
+          "Let's try beating the highscore! Keep going!";
   }
 
   playAgainButton.style.display = "block";
 }
 
-
 // Function to reset scores back to 0 for play again
-function resetScore () {
+function resetScore() {
+  correctScore = 0;
+  incorrectScore = 0;
 
-let correctScore = document.getElementById("correct-score-score");
-correctScore.innerText = 0;
+  let correctScoreElement = document.getElementById("correct-score-counter");
+  correctScoreElement.innerText = correctScore;
 
-let incorrectScore = document.getElementById("incorrect-score-coutner-score");
-incorrectScore.innerText = 0;
+  let incorrectScoreElement = document.getElementById(
+      "incorrect-score-counter"
+  );
+  incorrectScoreElement.innerText = incorrectScore;
 }
 
 // Functions to increment scores
 
-// Variables for scores
-let correctScore = 0;
-let incorrectScore = 0;
-
-
-function incrementCorrect() { 
-document.getElementById("correct-score-counter").innerText = ++correctScore;
+function incrementCorrect() {
+  document.getElementById("correct-score-counter").innerText = ++correctScore;
 }
 
 function incrementIncorrect() {
-document.getElementById("incorrect-score-counter").innerText = ++incorrectScore;
+  document.getElementById("incorrect-score-counter").innerText =
+      ++incorrectScore;
 }
 
+document.addEventListener("DOMContentLoaded", function name(params) {
 // Event Listeners
 if (document.getElementById("next-btn")) {
-startGame();
+    startGame();
 
-document.getElementById("next-btn").addEventListener("click", nextQuestion);
-document.getElementById("play-again-btn").addEventListener("click", resetScore);
-document.getElementById("play-again-btn").addEventListener("click", startGame);
+    document.getElementById("next-btn").addEventListener("click", nextQuestion);
+    document
+        .getElementById("play-again-btn")
+        .addEventListener("click", resetScore);
+    document
+        .getElementById("play-again-btn")
+        .addEventListener("click", startGame);
 }
+
+})
